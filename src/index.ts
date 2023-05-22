90b1a87a18945122b1f54ad98eebb54e6972bb22
Bryguy76 <bryant.bourgeois@comcast.net>
2023-05-21 22:08:03 -0600

    Wire up search button

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 import { currentWeather, weatherReport } from "./fetchCity";
 import { processCurrent, processForecast } from "./processData";
 
-const test = currentWeather("denver");
+// const test = currentWeather("denver");
 
-const test2 = weatherReport("shanghai");
-test.then((data) => console.log(data));
+// const test2 = weatherReport("shanghai");
+// test.then((data) => console.log(data));
 
-test.then((data) => processCurrent(data)).then((output) => console.log(output));
+// test.then((data) => processCurrent(data)).then((output) => console.log(output));
 
-test2.then((data) => console.log(data));
+// test2.then((data) => console.log(data));
 
-test2
-  .then((data) => processForecast(data))
-  .then((output) => console.log(output));
+// test2
+//   .then((data) => processForecast(data))
+//   .then((output) => console.log(output));
+
+const inputCity = document.querySelector("input");
+const submitCity = document.querySelector("button");
+
+submitCity.addEventListener("click", function () {
+  if (inputCity.value != "") {
+    const searchCurrent = currentWeather(inputCity.value);
+    searchCurrent
+      .then((data) => processCurrent(data))
+      .then((output) => console.log(output));
+    const searchForecast = weatherReport(inputCity.value);
+    searchForecast
+      .then((data) => processForecast(data))
+      .then((output) => console.log(output));
+  }
+});
